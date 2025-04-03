5b73c9f2837d61e1797b26eda91af8f2deee4b5e
21262 <21262webdev@gmail.com>
2025-04-03 18:27:49 +0530

    Basic Menu

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const width = window.innerWidth;
-const height = window.innerHeight;
+const width = 600;
+const height = 600;
 
-canvas.width = width;
-canvas.height = height;
+canvas.width = 600;
+canvas.height = 600;
 
 ctx.fillStyle = "#4DC2C6";
 ctx.fillRect(0, 0, width, height);
 
-ctx.fillStyle = "#F1EBAD";
-ctx.fillRect(0, height - (height / 10), width, height / 10);
-console.log(ctx);
+ctx.fillStyle = "#EFEDB0";
+ctx.fillRect(0, height - (height / 7), width, height / 7);
+
+ctx.font = "3.5rem 'Press Start 2P', system-ui";
+
+ctx.fillStyle = "#EFEDB2";
+ctx.fillText("FLAPPY", 20, 150);
+
+ctx.fillStyle = "#92E549";
+ctx.fillText("BIRD", 20, 240);
+
+ctx.lineWidth = 1.5;
+ctx.fillStyle = "#FAFDF5";
+ctx.strokeStyle = "#292D32";
+ctx.beginPath();
+ctx.roundRect(100, 250, 185, 120, 5);
+ctx.roundRect(340, 250, 185, 120, 5);
+ctx.roundRect(212, 385, 185, 120, 5);
+ctx.fill();
+ctx.stroke();
