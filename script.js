c649c419e2d0c50a8309e502ae8cf5f59bcc9895
21262 <21262webdev@gmail.com>
2025-04-03 20:10:33 +0530

    Update Menu

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
+import { pipe } from "./assets/pipe.js";
+
+const PIXEL_ARTS = { pipe, };
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = 600;
 const height = 600;
 
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = width;
+canvas.height = height;
 
-ctx.fillStyle = "#4DC2C6";
-ctx.fillRect(0, 0, width, height);
+function menu() {
+    //Clear the Canvas
+    ctx.clearRect(0, 0, width, height);
 
-ctx.fillStyle = "#EFEDB0";
-ctx.fillRect(0, height - (height / 7), width, height / 7);
+    //Draw The Sky 
+    ctx.fillStyle = "#4DC2C6";
+    ctx.fillRect(0, 0, width, height);
+    //Draw the bottom most yellow ground
+    ctx.fillStyle = "#EFEDB0";
+    ctx.fillRect(0, height - (height / 10), width, height / 10);
 
-ctx.font = "3.5rem 'Press Start 2P', system-ui";
+    //Use the Game's Font
+    ctx.font = "2.85rem 'Press Start 2P', system-ui";
+    
+    //The Stroke of text
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "#292D32";
+    ctx.strokeText("FLAPPY", 30, 130);
+    ctx.strokeText("BIRD", 30, 210);
 
-ctx.fillStyle = "#EFEDB2";
-ctx.fillText("FLAPPY", 20, 150);
+    //The word FLAPPY in Whitish-yellow
+    ctx.fillStyle = "#EFEDB2";
+    ctx.fillText("FLAPPY", 30, 130);
 
-ctx.fillStyle = "#92E549";
-ctx.fillText("BIRD", 20, 240);
+    //The word BIRD in Light-green
+    ctx.fillStyle = "#92E549";
+    ctx.fillText("BIRD", 30, 210);
+    
+    //Draw the 3 rectangles for Play, Shop and Leaderboard
+    ctx.lineWidth = 2.5;
+    ctx.fillStyle = "#FAFDF5";
+    ctx.beginPath();
+    ctx.roundRect(100, 220, 185, 140, 5);
+    ctx.roundRect(320, 220, 185, 140, 5);
+    ctx.roundRect(205, 375, 185, 145, 5);
+    ctx.fill();
+    ctx.stroke();
+}
 
-ctx.lineWidth = 1.5;
-ctx.fillStyle = "#FAFDF5";
-ctx.strokeStyle = "#292D32";
-ctx.beginPath();
-ctx.roundRect(100, 250, 185, 120, 5);
-ctx.roundRect(340, 250, 185, 120, 5);
-ctx.roundRect(212, 385, 185, 120, 5);
-ctx.fill();
-ctx.stroke();
+const renderAsset = (props, pixelArt, colorSet) => {
+    for (let row = 0; row < pixelArt.length; row++) {
+        for (let col = 0; col < pixelArt[row].length; col++) {
+            const {x, y, size} = props;
+            const character =  pixelArt[row][col];
+            if (character !== " ") {
+                ctx.fillStyle = colorSet[character];
+                ctx.fillRect(x + (col * size), y + (row  * size), size, size);
+            }
+        }
+    }
+    //First the array --> Find the first string in array ->  first character in the string -> x = 0 y = 0 size = 10
+    //First the arra --> Find the first string in array -> second character in the string -> x = 0, y = 10
+}
+
+menu();
+renderAsset({x: 500, y: 0, size: 2}, PIXEL_ARTS.pipe[0], PIXEL_ARTS.pipe[1]);
+renderAsset({x: -10, y: 350, size: 2}, PIXEL_ARTS.pipe[0].reverse(), PIXEL_ARTS.pipe[1]);
