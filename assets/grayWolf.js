e9a642edf89a866c31e1ddc537b0a9cf74eb569f
21262 <21262webdev@gmail.com>
2025-04-10 12:59:10 +0530

    Added Gray wolf bird

---
+++ assets/grayWolf.js
@@ -0,0 +1,30 @@
+export const grayWolf = [
+    [   
+        "         o   o",
+        "        oko oko",
+        "     ooookoookooooooo",
+        "    okkggggggggossssso",
+        "    okkggggggggosssssso",
+        " ooooooogggggggossssssso",
+        "osssssssoggggggosssessso",
+        "osssssssoggggggosssessso",
+        "okssssskoggggggosssessso",
+        "okssssskoggggggossssssso",
+        "okkkkkkkoggggggoooooooooo",
+        " oooooooggggggollllllllllo",
+        "   okkkkgggggoloooooooooo",
+        "    okkkkgggggolllllllllo",
+        "    okkkkkkggggooooooooo", 
+        "     ooooooooooooo",
+    ],
+    {
+        "o": "#4c382d",
+        "b": "#f2f4f5",
+        "c": "#e6e617",
+        "g": "#272624",
+        "k": "#62554d",
+        "e": "#4d6a7c",
+        "s": "#f2f1f6",
+        "l": "#8f8969"
+    }
+]
