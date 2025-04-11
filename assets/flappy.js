95a5a453dbc3a9653740f166541edc5d72620ecb
21262 <21262webdev@gmail.com>
2025-04-11 12:37:23 +0530

    Add Remake Flappy Bird

---
+++ assets/flappy.js
@@ -0,0 +1,34 @@
+export const flappy = [
+    [   
+        "       ooooooooo ", 
+        "      ooyyyyyowwoo",
+        "    oooyyyyyyowwwoo",
+        "  ooowyyyyyyyowwwwoo ",
+        " oowwwyyyyyyyowwbwwoo",
+        "oowwwwyyyyyyyowwbwwwo",
+        "ooooooooyyyyyowwbwwwo",
+        "oowwwwwwooyyyoowwwwwo",
+        "oowwwwwwooyyyoowwwwwo",
+        "oowwwwwwooyyyyyoowwwo",
+        "ooyyyyyyooyyyyyyyoooooo",
+        "ooooooooggggyyyyorrrrro",
+        " oggggggggggggggorrrrro",
+        " ogggggggggggggorrrrrro",
+        " oooogggggggggoorroooo",
+        "    ooooggggoorrrrroo",
+        "    ooooggggggooooo",
+        "     ooooooooo"
+
+
+    ],
+    {
+        "o": "#262221",
+        "y": "#FFEE32",
+        "w": "#EDF6F7",
+        "r": "#FB7150",
+        "b": "#423E3B",
+        "g": "#FFBA00",
+        "s": "#f2f1f6",
+        "l": "#8f8969"
+    }
+]
