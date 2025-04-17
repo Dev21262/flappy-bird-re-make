e95669ab1e96388e9f6532624035d1a9c3d62a12
21262 <21262webdev@gmail.com>
2025-04-17 10:01:40 +0530

    Add OG Flappy Bird Art

---
+++ assets/ogFlappy.js
@@ -0,0 +1,44 @@
+export const ogFlappy = [
+    [   
+        [   
+            "        bbbbb",
+            "     bbbzzzbwb",
+            "    bzzzyybwwwb",
+            "   bzyyyyybzwwwb      ",
+            "  byyyyyyybzwwbwb     ",
+            "  byyyyyyybzwwbwb     ",
+            "  byyyyyyyybzwwwb      ",
+            " bbbbbbyyyybbbbbbb",
+            "byzzzyybyybrrrrrrrb",
+            "bzzzzyboobrrbbbbbb",
+            "bzzzyboooobrrrrrrb",
+            " bbbbbooooobbbbbb",
+            "     bbbbbb",
+        ],
+        [   
+            "        bbbbb",
+            "     bbbzzzbwb",
+            "    bzzzyybwwwb",
+            "   bzyyyyybzwwwb      ",
+            "  byyyyyyybzwwbwb     ",
+            "  byyyyyyybzwwbwb     ",
+            "  byyyyyyyybzwwwb      ",
+            " bbbbbyyyyybbbbbbb",
+            "bzzzzzbyyybrrrrrrrb",
+            "byzzzyboobrrbbbbbb",
+            " byyyboooobrrrrrrb",
+            " bbbboooooobbbbbb",
+            "     bbbbbb",
+        ],
+    ],
+    {
+        "y": "#D4BF27",
+        "b": "#533846",
+        "c": "#e6e617",
+        "w": "#DDE2B1",
+        "r": "#EA5040",
+        "e": "#4d6a7c",
+        "z": "#C8C0C0",
+        "o": "#E48116"
+    }
+]
