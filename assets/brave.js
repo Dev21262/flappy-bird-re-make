e53cdd897db0a552518482d5376e91f2855219f0
21262 <21262webdev@gmail.com>
2025-04-25 10:13:44 +0530

    New Bird

---
+++ assets/brave.js
@@ -0,0 +1,44 @@
+export const brave = [
+    [   
+        [   
+            "   bbbbbbbbbbb",
+            "    boooobbooob",
+            "    bborbzzbbrrb",
+            "   borrrbzwwwbbrb ",
+            "  borrrrbzwwwwbbbb",
+            "  borrrrrbzzwwbwwb",
+            "bbbbbbbrrrbzwwwwwb",
+            " bwwwzbrrrrbbbbbbbb",
+            " bwzzzbrrrrrbyyyyyb",
+            "  bbbberrrrbyybbbbb",
+            "   beeeerrrrbyyyyb",
+            "    bbeeerrrrbbbbb",
+            "      bbbbbbb",
+        ],
+        [   
+            "   bbbbbbbbbbb",
+            "    boooobbooob",
+            "    bborbzzbbrrb",
+            "   borrrbzwwwbbrb",
+            "  borrrrbzwwwwbbbb",
+            "  borrrrrbzzwwbwwb",
+            " bbbbbrrrrbzwwwwwb",
+            " bwwwwbrrrrbbbbbbbb",
+            " bwwwwbrrrrrbyyyyyb",
+            "  bbbberrrrbyybbbbb",
+            "   beeeerrrrbyyyyb",
+            "    bbeeerrrrbbbbb",
+            "      bbbbbbb",
+        ],
+    ],
+    {
+        "y": "#EDE932",
+        "b": "#523A48",
+        "c": "#e6e617",
+        "w": "#FFFFFF",
+        "r": "#FD3A00",
+        "e": "#D13A03",
+        "z": "#CCD5C0",
+        "o": "#FF8B0F"
+    }
+]
